36bb09261871a3e49a44066e73e7380fa59312a8
jbitsind <jbitsind@gmail.com>
2025-04-25 04:07:58 +0200

    Style-switcher modification

--- src/js/style-switcher.js
+++ src/js/style-switcher.js
@@ -1,45 +1,24 @@
-/* ========================== Toggler Style Switcher ========================== */
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+    const dayNight = document.querySelector(".day-night");
+    const alternateStyles = document.querySelectorAll('link[rel="alternate stylesheet"][title]');
+
+    // Toggle panel
+    styleSwitcherToggle.addEventListener("click", () => {
         document.querySelector(".style-switcher").classList.toggle("open");
+    });
 
-    })
-    // Close style-switcher on scroll
-window.addEventListener("scroll", () => {
-        if (document.querySelector(".style-switcher").classList.contains("open")) {
-            document.querySelector(".style-switcher").classList.remove("open");
-        }
-    })
-    /* ========================== Theme Colors ========================== */
-const alternateStyles = document.querySelectorAll('link[rel="alternate stylesheet"][title]');
+    // Light/dark toggle
+    dayNight.addEventListener("click", () => {
+        // …
+    });
 
-function setActiveStyle(color) {
-    alternateStyles.forEach((style) => {
-        if (color === style.getAttribute("title")) {
-            style.removeAttribute("disabled");
-        } else {
-            style.setAttribute("disabled", "true");
-        }
-    })
-}
-/* ========================== Theme Light and Dark Mode ========================== */
-const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
-    dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
-
-    // Save the current theme in local storage
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
-    }
-})
-window.addEventListener("load", () => {
-    if (document.body.classList.contains("dark")) {
-        dayNight.querySelector("i").classList.add("fa-sun");
-    } else {
-        dayNight.querySelector("i").classList.add("fa-moon");
-    }
-})
+    // Color picks
+    window.setActiveStyle = function(color) {
+        alternateStyles.forEach(style => {
+            style.title === color ?
+                style.removeAttribute("disabled") :
+                style.setAttribute("disabled", "true");
+        });
+    };
+});
