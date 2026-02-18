5152eb3ccf7b51388137a75d309fb1fca90c0fd3
jbitsind <jbitsind@gmail.com>
2026-02-18 21:56:17 +0200

    chore(build): include built dist for Pages deployment

---
+++ dist/Tsk_Portfolio-main.9ad26ee3.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".skill-category .category-header").forEach(e=>{e.addEventListener("click",()=>{let t=e.parentElement;t.classList.toggle("open"),document.querySelectorAll(".skill-category").forEach(e=>{e!==t&&e.classList.remove("open")})})})});
+//# sourceMappingURL=Tsk_Portfolio-main.9ad26ee3.js.map
